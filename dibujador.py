6ef388e995679ce23c89e36053924e07ba34576b
Bau-ti <b.antunez@alumno.etec.um.edu.ar>
2026-03-30 08:51:52 -0300

    el DIBUJADOR

---
+++ dibujador.py
@@ -0,0 +1,35 @@
+print("Hola, decime que dibujar:")
+while(True):
+    dibujo=input("\nUn gato(1), un perro(2), una compu(3):" )
+    if dibujo == "1":
+        print("   |\__/,|   (`\ ")
+        print("   |o o  |__ _) ")
+        print(" _.( T   )  `  /")
+        print("((_ `^--' /_<  \ ")
+        print("`` `-'(((/  (((/")
+    elif dibujo == "2":
+        print("   |\|\ ")
+        print("  ..    \       . ")
+        print("o--     \\    / @)")
+        print("v__///\\\\__/ @")
+        print("   {           }")
+        print("    {  } \\\{  }")
+        print("   <_|      <_|")
+    elif dibujo == "3":
+        print("___________________")
+        print("| _______________ |")
+        print("| |XXXXXXXXXXXXX| |")
+        print("| |XXXXXXXXXXXXX| |")
+        print("| |XXXXXXXXXXXXX| |")
+        print("| |XXXXXXXXXXXXX| |")
+        print("| |XXXXXXXXXXXXX| |")
+        print("|_________________|")
+        print("    _[_______]_")
+        print("___[___________]___")
+        print("|    [_____] []|__")
+        print("|    [_____] []|  \__")
+        print("L___________________J")
+    else:
+        print("proba de nuevo pancho...")
+
+       
